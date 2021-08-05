7f93d08a8d16444f84b6ab3f75a06ba2933b5bd3
jhonatan <jhonatanpires636@gmail.com>
2021-08-05 08:07:46 -0300

    Initial commit

---
+++ src/main/java/com/CursoAlgaWorks/CursoAlgaWorksApplication.java
@@ -0,0 +1,13 @@
+package com.CursoAlgaWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoAlgaWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoAlgaWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CursoAlgaWorks/CursoAlgaWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CursoAlgaWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoAlgaWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
