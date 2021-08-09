6f8d9d903cc446c56d2d52a601b5c25c31bc584a
jhonatan <jhonatanpires636@gmail.com>
2021-08-09 12:05:49 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/CursoAlgaWorks/Factory/Contato.java

--- src/main/java/com/CursoAlgaWorks/Controller/ContatosControle.java
+++ src/main/java/com/CursoAlgaWorks/Controller/ContatosControle.java
@@ -30,7 +30,6 @@ public class ContatosControle {
         modelAndView.addObject("contatos", LISTA_CONTATOS);
         return modelAndView;
     }
-
     @GetMapping("/contatos/novo")
     public ModelAndView novo(){
         ModelAndView modelAndView = new ModelAndView("formulario");
--- src/main/java/com/CursoAlgaWorks/Factory/Contato.java
+++ src/main/java/com/CursoAlgaWorks/Factory/Contato.java
@@ -19,22 +19,4 @@ public class Contato {
     public boolean isNovo(){
         return id == null;
     }
-   /* public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }*/
 }
